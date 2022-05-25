cbe0305a040f7b9fe3a9f631a0a707e8e868b344
igoreta <ivoronline@gmail.com>
2022-05-25 15:31:24 +0200

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_filter_exclude/SpringbootFilterExcludeApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_filter_exclude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFilterExcludeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootFilterExcludeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ivoronline/springboot_filter_exclude/SpringbootFilterExcludeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_filter_exclude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFilterExcludeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
